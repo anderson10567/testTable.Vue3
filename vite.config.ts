d3caa4502f84ebb84ac23a203e5785c3b4f38ec9
anderson10567 <grisinandrej476@gmail.com>
2025-10-30 16:30:46 +0300

    init

--- vite.config.ts
+++ vite.config.ts
@@ -4,9 +4,10 @@ import vueJsx from '@vitejs/plugin-vue-jsx'
 import path from 'node:path'
 
 const repoName = process.env.GITHUB_REPOSITORY?.split('/')[1]
+const isUserSite = repoName?.endsWith('.github.io')
 
 export default defineConfig({
-	base: repoName ? `/${repoName}/` : '/',
+    base: repoName ? (isUserSite ? '/' : `/${repoName}/`) : '/',
 	plugins: [vue(), vueJsx()],
 	resolve: {
 		alias: {
